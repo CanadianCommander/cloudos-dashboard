7ae31fa8d0a1df3cae561d950f30ccf149a23c40
CanadianCommander <ben.benetti@shaw.ca>
2019-09-23 21:40:24 -0700

    UI ++ + add material cdk + make use sidenav cdk component for the side nav + import fonts + play with layout + start filling in color sheet

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -6,11 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  statusBarPosition  = STATUS_BAR_POSITION.LEFT;
-  statusBarPositions = STATUS_BAR_POSITION;
-}
 
-enum STATUS_BAR_POSITION {
-  LEFT,
-  RIGHT,
 }
--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppGridComponent } from './app-grid/app-grid.component';
 import {StatusbarComponent} from './statusbar/statusbar.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {MatSidenavModule} from "@angular/material";
 
 @NgModule({
   declarations: [
@@ -14,7 +16,11 @@ import {StatusbarComponent} from './statusbar/statusbar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    // material cdk
+    BrowserAnimationsModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
